63b1d333592f31db1a6bb9e97621d769d51d70b2
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-16 20:54:00 -0300

    :sparkles: feat: Implementando método de busca dos jogos desenvolvidos

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -2,27 +2,37 @@ namespace TrybeGames;
 
 public class TrybeGamesDatabase
 {
-    public List<Game> Games = new List<Game>();
+  public List<Game> Games = new List<Game>();
 
-    public List<GameStudio> GameStudios = new List<GameStudio>();
+  public List<GameStudio> GameStudios = new List<GameStudio>();
 
-    public List<Player> Players = new List<Player>();
+  public List<Player> Players = new List<Player>();
 
-    public List<Game> GetGamesDevelopedBy(GameStudio gameStudio)
+  public List<Game> GetGamesDevelopedBy(GameStudio gameStudio)
+  {
+    // implementar
+    var games = new List<Game>();
+
+    foreach (var game in Games)
     {
-        // implementar
-        throw new NotImplementedException();
+      if (game.DeveloperStudio == gameStudio.Id)
+      {
+        games.Add(game);
+      }
     }
 
-    public List<Game> GetGamesPlayedBy(Player player)
-    {
-        // Implementar
-        throw new NotImplementedException();
-    }
+    return games;
+  }
 
-    public List<Game> GetGamesOwnedBy(Player playerEntry)
-    {
-        // Implementar
-        throw new NotImplementedException();
-    }
+  public List<Game> GetGamesPlayedBy(Player player)
+  {
+    // Implementar
+    throw new NotImplementedException();
+  }
+
+  public List<Game> GetGamesOwnedBy(Player playerEntry)
+  {
+    // Implementar
+    throw new NotImplementedException();
+  }
 }
